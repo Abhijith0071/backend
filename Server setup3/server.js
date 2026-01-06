f970369d102fad1a6ca221b028d505c662277953
abhijith <abhijithajithkumar32@gmail.com>
2026-01-06 09:54:04 +0530

    Server setup 3

---
+++ Server setup3/server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+
+const app=express();
+
+const port=5000;
+
+app.get ('/',(req,res)=>{
+    res.send('Hello from Server 3!');
+})
+
+app.listen(
+    port,()=>{
+        console.log(`running on port:${port}`)
+    }
+)
